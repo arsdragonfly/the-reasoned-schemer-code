ef723f199e1d917c785db709609c6fecc84cc94c
Zheyu Shen <arsdragonfly@gmail.com>
2020-12-21 23:46:02 +0800

    update

--- chp2.rkt
+++ chp2.rkt
@@ -1,3 +1,7 @@
 #lang racket
 (provide (all-defined-out) (all-from-out "chp1.rkt"))
 (require "chp1.rkt")
+(defrel (nullo x) (== '() x))
+(defrel (conso a d p) (== `(,a . ,d) p))
+(defrel (caro a d) (fresh (l) (conso d l a)))
+(defrel (cdro a d) (fresh (l) (conso l d a)))
--- chp3.rkt
+++ chp3.rkt
@@ -1,3 +1,4 @@
 #lang racket
 (provide (all-defined-out) (all-from-out "chp2.rkt"))
 (require "chp2.rkt")
+(defrel (listo l) (conde ((nullo l)) ((fresh (d) (cdro l d) (listo d)))))
